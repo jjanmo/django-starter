a43684b45a6ad8b96739180c91eca07da13b327d
jjanmo <jjanmo07@hanmail.net>
2021-02-07 20:46:14 +0900

    Update view using template

--- start/views.py
+++ start/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
+import random
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse('Hello World')
+    # template = loader.get_template('index.html')
+    now = datetime.now()
+    context = {
+        'current_date': now
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)  # 위에 코드를 아래와 같이 쓸 수 있다 : shortcut
+
+
+def select(request):
+    number = random.randrange(1, 11)
+    context = {
+        'number': number
+    }
+    return render(request, 'select.html', context)
+
+
+def result(request):
+    context = {
+        'numbers': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+    return render(request, 'result.html', context)
