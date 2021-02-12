acc48ed8b56176ca5f84dfb09962ca06ec7d2af0
jjanmo <jjanmo07@hanmail.net>
2021-02-12 20:06:14 +0900

    Save data using ModelForm

--- model_app/urls.py
+++ model_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('list/', views.print_list, name="print_list"),
-    path('form/', views.create_form, name="create_form"),
-    path('confirm/', views.confirm, name="confirm")
+    # path('form/', views.create_form, name="create_form"),
+    path('form/', views.form, name="form"),
+    # path('confirm/', views.confirm, name="confirm")
 ]
--- model_app/views.py
+++ model_app/views.py
@@ -11,15 +11,24 @@ def print_list(request):
     return render(request, 'model_app/list.html', context)
 
 
-def create_form(request):
+# def create_form(request):
+#     form = PostForm()
+#     return render(request, 'model_app/form.html', {'form': form})
+
+
+# def confirm(request):
+#     form = PostForm(request.POST)
+#     if form.is_valid():
+#         return render(request, 'model_app/confirm.html', {'form': form})
+#     return HttpResponseRedirect('/model_app/form/')
+
+
+def form(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_item = form.save()
+            return HttpResponseRedirect('/model_app/list/')
+
     form = PostForm()
     return render(request, 'model_app/form.html', {'form': form})
-
-
-def confirm(request):
-    form = PostForm(request.POST)
-    if form.is_valid():
-        print('hahahah')
-        return render(request, 'model_app/confirm.html', {'form': form})
-    print('heheheheh')
-    return HttpResponseRedirect('/model_app/form/')
