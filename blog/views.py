d11f6e0d7cfb156deffb47151f6f08f06564e977
jjanmo <jjanmo07@hanmail.net>
2021-02-14 17:16:57 +0900

    Add pagination

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from blog.models import Cafe
+from django.core.paginator import Paginator
 
 
 def list(request):
+    cafes = Cafe.objects.all()
+    paginator = Paginator(cafes, 5)
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+    print('items', items)
     context = {
-        'cafes': Cafe.objects.all()
+        'cafes': items
     }
     return render(request, 'blog/list.html', context)
