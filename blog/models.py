eb68a5519db8d92a6fdfa2781dfd3dc4760a19f4
jjanmo <jjanmo07@hanmail.net>
2021-02-20 20:58:20 +0900

    Make review model and migratite model

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,13 @@ class Cafe(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Review(models.Model):
+    point = models.IntegerField()
+    comment = models.CharField(max_length=300)
+
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)  # 카페의 정보가 삭제되면 따라서 같이 삭제 되겠다는 의미
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
